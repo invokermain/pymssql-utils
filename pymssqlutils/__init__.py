aca6b07329c2c128b0af0c39ad7bdd2f657a645b
Tim OSullivan <tim.osullivan@accounttechnologies.com>
2021-01-21 16:16:16 +0000

    Use explicit imports to tidy package namespace

--- pymssqlutils/__init__.py
+++ pymssqlutils/__init__.py
@@ -1,2 +1,2 @@
-from .methods import *
-from .databaseresult import *
+from .databaseresult import DatabaseResult, DatabaseError
+from .methods import execute, execute_many, query, to_sql_list, model_to_values
