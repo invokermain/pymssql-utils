70a36cd37acaf50813e851b7dd6c5b7c26033f12
Tim OSullivan <tim.osullivan@accounttechnologies.com>
2021-01-21 17:32:42 +0000

    (Regression) adding substitute_params back in to module namespace

--- pymssqlutils/__init__.py
+++ pymssqlutils/__init__.py
@@ -1,2 +1,9 @@
 from .databaseresult import DatabaseResult, DatabaseError
-from .methods import execute, execute_many, query, to_sql_list, model_to_values
+from .methods import (
+    execute,
+    execute_many,
+    query,
+    to_sql_list,
+    model_to_values,
+    substitute_parameters,
+)
