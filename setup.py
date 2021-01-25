fe3531221feead24078c19e0fb53cf947aa292ad
Tim OSullivan <44192816+invokermain@users.noreply.github.com>
2021-01-25 14:50:41 +0000

    Update setup.py
    bumping version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pymssql-utils",
-    version="0.0.8",
+    version="0.0.9",
     description="pymssql-utils is a small library that wraps pymssql to make your life easier.",
     long_description=(pathlib.Path(__file__).parent / "README.md").read_text(),
     long_description_content_type="text/markdown",
