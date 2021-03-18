86e254a2bbda32d3aaab0d5f54ce11d68915ca1d
Tim OSullivan <tim.osullivan@accounttechnologies.com>
2021-03-18 20:52:15 +0000

    Specified __all__ & added set_connection_details

--- pymssqlutils/__init__.py
+++ pymssqlutils/__init__.py
@@ -5,4 +5,16 @@ from .methods import (
     to_sql_list,
     model_to_values,
     substitute_parameters,
+    set_connection_details,
 )
+
+__all__ = [
+    "execute",
+    "query",
+    "to_sql_list",
+    "model_to_values",
+    "substitute_parameters",
+    "set_connection_details",
+    "DatabaseResult",
+    "DatabaseError",
+]
